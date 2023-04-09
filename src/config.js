d502d786befb3bfe8a00078354d67e343fa1d8f4
Adil Hussain <adil.cs.work@gmail.com>
2023-04-09 19:19:50 +0530

    site changed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL="https://theonlinecustomersupport.com/api/v1";
+export const BASE_URL="https://getcustomersupportpoint.com/api/v1";
 // export const BASE_URL="http://82.180.163.106/api/v1";
 
